bc2359da3b4530edee12461b4a9f3d3f2244b2ef
Anh Hồ <anhho@Anhs-MacBook-Pro.local>
2023-03-12 15:10:25 +0700

    feat(): init pj

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>default layout</h1>
+    <div>header</div>
+    <slot />  
+    <div>footer</div>
+  </div>
+</template>
+
+<script setup>
+</script>
---
+++ pages/posts/[id].vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        auth
+    </div>
+</template>
+<script setup>
+
+const route = useRoute()
+// When accessing /posts/1, route.params.id will be 1
+console.log(route.params.id)
+</script>
